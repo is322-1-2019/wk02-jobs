fe3f209a9a220bd005229c01d8da5908875daa92
Krittanon Yuangthong <axzess@gmail.com>
2019-08-28 16:31:31 +0700

    Monster Hunter

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,41 @@
 <template>
   <div class="main">
-    <h1>Hello, World!</h1>
+    <h1>Monster Hunter</h1>
 
-    <p>test, test and test</p>
+    <p>There are 2 clans for you to choose!</p>
 
-    <h3>By Jobs Krittanon</h3>
+    <div class="row">
+      <div class="col-md-6" id="player1">
+        <div class="card">
+          <div class="card-header">
+            <h3>Hunter</h3>
+          </div>
+          <div class="card-body">
+            <img id="hunter" src="../assets/img/hunter.png" />
+          </div>
+          <div class="card-footer">
+            <p>First is Hunters the good man who were born to protect the world.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-6" id="player2">
+        <div class="card">
+          <div class="card-header">
+            <h3>Monster</h3>
+          </div>
+          <div class="card-body">
+            <img id="hunter" src="../assets/img/monster.jpg" />
+          </div>
+          <div class="card-footer">
+            <p>Next is Monster the evil creatures that will devour all the life and bring chaos to the world.</p>
+          </div>
+        </div>
+      </div>
+
+      </div>
+
+    <h3>Game Design by Jobs Krittanon</h3>
   </div>
 </template>
 
@@ -33,4 +64,29 @@ li {
 a {
   color: #42b983;
 }
+
+#hunter {
+  width: 200px;
+  height: 250px;
+}
+
+.card {
+
+ text-align: center;
+ vertical-align: middle;
+
+}
+
+.card :hover { 
+
+  background-color: lightblue;
+  cursor: pointer;
+}
+
+@media only screen and (max-width: 600px) {
+  .card {
+    background-color: red;
+  }
+}
+
 </style>
