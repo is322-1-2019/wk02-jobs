b77d13fd6e4f8fb85b7f010f9812df39a2ee628e
Krittanon Yuangthong <axzess@gmail.com>
2019-08-28 15:26:09 +0700

    remove logo updated

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,107 +1,20 @@
-
-
 <template>
-  <div class="container">
-    
-    <header>
+  <div class="main">
+    <h1>Hello, World!</h1>
 
-        <center>/////////////////////////HEADER/////////////////////////</center>
+    <p>test, test and test</p>
 
-    </header>
-
-    <div class="row">
-      <div class="col-md-2">
-        <h3>Column 1</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-      </div>
-      <div class="col-md-3">
-        <h3>Column 2</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-      </div>
-      <div class="col-md-7">
-        <h3>Column 3</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-      </div>
-    </div>
-
-    <h2>Bordered Table</h2>
-  <p>The .table-bordered class adds borders on all sides of the table and the cells:</p>            
-  <table class="table table-striped">
-    <thead>
-      <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-      </tr>
-    </tbody>
-  </table>
-
-<div class="spinner-border"></div>
-
-
-    <form>
-
-      name : <input type='text'>
-     
-      <button>Accept</button>
-     </form> 
-
-<div class="container">
-  <h2>Basic Card</h2>
-  <div class="card">
-    <div class="card-body">Basic card</div>
+    <h3>By Jobs Krittanon</h3>
   </div>
-</div>
-
-      <footer>
-
-        <center>/////////////////////////footer/////////////////////////</center>
-
-      </footer>
-   
-   <br><br>
-   
-   </div> 
-</template>
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
